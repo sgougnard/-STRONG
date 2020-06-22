bce3e87cfc340c4756bf9fbefc497fbd8c1eff0e
Simon Gougnard <simongougnard@gmail.com>
2020-06-23 00:52:35 +0200

    added home pge and css on it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @workouts = Workout.all.order(created_at: :desc).first(6)
+    # @workout = Workout.find(params[:id])
   end
 
 end
